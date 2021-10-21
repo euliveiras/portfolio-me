68f00431fcc321e25c1a1a23c09154700797b28a
matheus oliveira <matheus.skm@hotmail.com>
2021-10-21 19:50:10 -0300

    ajustes de responsividade

--- src/components/sectionPage.tsx
+++ src/components/sectionPage.tsx
@@ -15,7 +15,7 @@ export const SectionPage: React.FC = () => {
         >
             <Flex
                 borderRadius="4"
-                h="90%"
+                h={["auto", "90%"]}
                 w="90%"
                 backgroundColor="white"
                 align="center"
@@ -23,13 +23,13 @@ export const SectionPage: React.FC = () => {
                 flexDir="column"
                 color="blue.900"
             >
-                <Text m="0 15% 5%" fontFamily="body" fontSize={[, "sm", "xl"]}>
+                <Text m="0 15% 5%" fontFamily="body" fontSize={["sm", "xl"]}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor vitae purus faucibus ornare suspendisse sed. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit. Cursus vitae congue mauris rhoncus aenean vel elit. Augue lacus viverra vitae congue eu. Quisque egestas diam in arcu cursus euismod quis. Elit ullamcorper dignissim cras tincidunt lobortis feugiat. Neque convallis a cras semper auctor neque vitae tempus. Quis varius quam quisque id diam vel. Et ultrices neque ornare aenean euismod elementum nisi quis. Elementum sagittis vitae et leo duis ut. Nunc aliquet bibendum enim facilisis gravida neque.
                 </Text>
-                <Text as="h2" fontSize="xx-large">
+                <Text as="h2" fontSize={["x-large", "xx-large"]}>
                     Informações de contato
                 </Text>
-                <VStack spacing={4} as="ul">
+                <VStack spacing={[1, 4]} as="ul">
                     <Text d="flex" alignItems="center" as="li"><Icon as={MdOutlineMail} boxSize="40px" pr="4" />
                         matheus.skm@hotmail.com
                     </Text>
